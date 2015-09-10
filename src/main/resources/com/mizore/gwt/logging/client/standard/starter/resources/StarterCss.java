0439d917c08daa68c050051d90913d5c9cbde656
Steeve Vandecappelle <me@mizore.fr>
2015-09-10 11:20:58 +0200

    Adding resources into src/main/resources

---
+++ src/main/resources/com/mizore/gwt/logging/client/standard/starter/resources/StarterCss.java
@@ -0,0 +1,23 @@
+package com.mizore.gwt.logging.client.standard.starter.resources;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface StarterCss extends CssResource {
+
+    String starter();
+
+    String icon();
+
+    String handle();
+
+    String on();
+
+    String red();
+
+    String green();
+
+    String label();
+
+    String title();
+
+}
